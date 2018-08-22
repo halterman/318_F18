b9e8ab38ec83938c69840ed1baac261df43ab0d2
halterman <halterman@southern.edu>
2018-08-22 13:28:37 -0400

    Version: Wed Aug 22 13:28:34 USEDT 2018

---
+++ 2018-08-22/CPTR_318_Test_2.cpp
@@ -0,0 +1,14 @@
+// Again, nothing really to see here.  We wrote this
+// simple program within the context of a custom
+// Visual Studio project template.  The video on
+// Eclass for the details about how to make a 
+// custom Visual Studio 2017 project template.
+
+// CPTR_318_Test_2.cpp
+// Created 8/22/2018 10:17:51 AM
+
+#include <iostream>
+
+int main() {
+	std::cout << "Test #2\n";
+}
