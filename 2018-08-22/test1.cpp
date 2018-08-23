b324bbc7171852b8a35490a114613d22528c9dfa
halterman <halterman@southern.edu>
2018-08-23 16:46:34 -0400

    Version: Thu Aug 23 16:46:17 USEDT 2018

--- 2018-08-22/test1.cpp
+++
@@ -1,9 +0,0 @@
-// Nothing really to see here.  We wrote this simple 
-// program while exploring project options in Visual
-// Studio 2017 (linker subsystem, compiler warnings,
-// and language standard).
-#include <iostream>
-
-int main() {
-	std::cout << "CPTR 318 Test program\n";
-}
