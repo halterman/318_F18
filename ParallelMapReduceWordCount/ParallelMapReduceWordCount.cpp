90e4b50d5b2e1a3ec1c42bc4dd6a9ad4f196366a
halterman <halterman@southern.edu>
2018-12-03 15:25:39 -0500

    Version: Mon Dec  3 15:25:35 USEST 2018

---
+++ ParallelMapReduceWordCount/ParallelMapReduceWordCount.cpp
@@ -0,0 +1,74 @@
+// Adapted from https://msdn.microsoft.com/en-us/library/hh873179.aspx
+
+#include <ppl.h>          // Parallel Patterns Library
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <unordered_map>
+
+using std::unordered_map, std::string, std::vector, std::pair;
+using concurrency::parallel_transform, concurrency::parallel_reduce;
+
+class MapFunc {
+public:
+	unordered_map<string, size_t> operator()(vector<string>& elements) const {
+		unordered_map<string, size_t> m;
+		for_each(begin(elements), end(elements),
+			[&m](const string& elem) { m[elem]++; });
+		return m;
+	}
+};
+
+
+struct ReduceFunc {
+	unordered_map<string, size_t> operator() (
+		const unordered_map<string, size_t>& x,
+		const unordered_map<string, size_t>& y) const {
+		unordered_map<string, size_t> ret(x);
+		for_each(begin(y), end(y), [&ret](const std::pair<string, size_t>& pr) {
+			auto key = pr.first;
+			auto val = pr.second;
+			ret[key] += val;
+		});
+		return ret;
+	}
+};
+
+
+int main() {
+	// File 1 
+	vector<string> v1;
+	v1.push_back("word1"); //1 
+	v1.push_back("word1"); //2 
+	v1.push_back("word2");
+	v1.push_back("word3");
+	v1.push_back("word4");
+
+	// File 2 
+	vector<string> v2;
+	v2.push_back("word5");
+	v2.push_back("word6");
+	v2.push_back("word7");
+	v2.push_back("word8");
+	v2.push_back("word1"); //3 
+
+	vector<vector<string>> v;
+	v.push_back(v1);
+	v.push_back(v2);
+
+	vector<unordered_map<string, size_t>> map(v.size());
+
+	// The Map operation
+	parallel_transform(begin(v), end(v), begin(map), MapFunc());
+
+	// The Reduce operation 
+	unordered_map<string, size_t> result = parallel_reduce(
+		begin(map), end(map), unordered_map<string, size_t>(), ReduceFunc());
+
+	std::cout << "\"word1\" occurs " << result["word1"] << " times. \n";
+}
+/* Output:
+"word1" occurs 3 times.
+*/
